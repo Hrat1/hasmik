3086704cb0df9ae6b5e6adaaa98a0e1a032f1928
Hrat1 <hrat.tahmazyan99@gmail.com>
2021-04-18 13:01:04 +0400

    *Created sendingMail function *Added sendingMail credentials

---
+++ js/sendEmail.js
@@ -0,0 +1,11 @@
+function sendEmail(to, subject, body){
+    Email.send({
+        SecureToken : "0db848e5-1280-4495-b751-0c0bb0be82c7",
+        To : `${to}`,
+        From : "hratjan99@gmail.com",
+        Subject : `${subject}`,
+        Body : `${body}`
+    }).then(
+        message => alert(message)
+    );
+}
