4d91fe47023fd2af00ce096f5a8613b99a5af7f1
Hrat1 <hrat.tahmazyan99@gmail.com>
2021-04-30 13:45:45 +0400

    *Added lessons view side for students

--- dashboard/userTypes/student/index.php
+++ dashboard/userTypes/student/index.php
@@ -1,2 +1,14 @@
-<?php
-echo "student side";
+<div id="teacherSide" class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="my-lessons-header">
+                <?php echo $lessonTypeToString;?> Lessons
+            </div>
+        </div>
+        <div class="col-12">
+            <div id="lessonListWrapper" class="lessons-list-wrapper">
+                <?php include_once "../operations/checkOperations/checkLessons.php"?>
+            </div>
+        </div>
+    </div>
+</div>
